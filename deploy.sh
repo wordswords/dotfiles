55680df1cd82c469c64905f8cd2b36a9e80188ff
David Craddock <contact@davidcraddock.net>
2016-11-04 14:29:50 +0000

    Modified deploy.sh as I'm now using oh-my-zsh

--- deploy.sh
+++ deploy.sh
@@ -7,22 +7,12 @@ cp -R ~/.vim ~/.olddotfiles/
 cp ~/.vimrc ~/.olddotfiles/.vimrc
 cp ~/.bash_profile ~/.olddotfiles/.bash_profile
 
-if [ -e "~/.config/fish/config.fish" ]; then
-  cp ~/.config/fish/config.fish ~/.olddotfiles/config.fish
-fi
-
 # remove
 
 rm -rf ~/.vim
 rm -f ~/.vimrc
 rm -f ~/.bash_profile
 rm -f ~/.config/fish/config.fish
-
-# deploy
-if [ -e "/usr/local/bin/fish" ]; then
-  mkdir -p ~/.dotfiles/config.fish 
-  ln -s ~/.dotfiles/config.fish ~/.config/fish/config.fish
-fi
 
 ln -s ~/.dotfiles/.vim ~/.vim
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
