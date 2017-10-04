32e2abf4ceeccd64c4607dc009d496e54c13ff48
David Craddock <david.craddock@arm.com>
2017-10-04 17:51:20 +0100

    Proper remote home

--- bin/deploy-remote-home.sh
+++ bin/deploy-remote-home.sh
@@ -4,28 +4,35 @@
 ### Script to copy local public ssh key and ~/.bash_profile_remote file to a given host
 ### and setup an alias in ~/.zsh_aliases and ~/.bash_aliases for quick keyed login
 ##
-
 source ~/.bash_aliases
-echo "Hostname or IP address of machine to deploy to. (Must have a user account named ${USER} already setup):"
-read HOST_TO_DEPLOY
-echo "Current list of aliases:"
-echo
-alias
-echo
-echo "Pick a short memorable unique bash and zsh alias for this new host:"
-read ALIAS
-echo "Shall we go ahead deploying to ${USER}@${HOST_TO_DEPLOY} and memorable alias: ${ALIAS}? (y/N)"
-read answer
-if echo "${answer}" | grep -iq "^y" ;then
-  echo "Proceeding..";
+if [ $# -gt 0 ]; then
+  HOST_TO_DEPLOY=$1
+  ALIAS=$2
 else
-  echo "Quitting..";
-  exit;
+  echo "Hostname or IP address of machine to deploy to. (Must have a user account named ${USER} already setup):"
+  read HOST_TO_DEPLOY
+  echo "Current list of aliases:"
+  echo
+  alias
+  echo
+  echo "Pick a short memorable unique bash and zsh alias for this new host:"
+  read ALIAS
+  echo "Shall we go ahead deploying to ${USER}@${HOST_TO_DEPLOY} and memorable alias: ${ALIAS}? (y/N)"
+  read answer
+  if echo "${answer}" | grep -iq "^y" ;then
+    echo "Proceeding..";
+  else
+    echo "Quitting..";
+    exit;
+  fi
 fi
+
 ssh "${USER}@${HOST_TO_DEPLOY}" "mkdir -p ~/.ssh"
-scp ~/.bash_profile_remote "${USER}@${HOST_TO_DEPLOY}:~/.bash_profile"
-scp ~/.vimrc "${USER}@${HOST_TO_DEPLOY}:~/"
-rsync -ave ssh ~/.vim "${USER}@${HOST_TO_DEPLOY}:~/"
+rsync -ave ssh ~/.dotfiles "${USER}@${HOST_TO_DEPLOY}:~/"
+ssh "${USER}@${HOST_TO_DEPLOY}" 'ln -s ${HOME}/.dotfiles/.vim ${HOME}/.vim'
+ssh "${USER}@${HOST_TO_DEPLOY}" 'ln -s ${HOME}/.dotfiles/.vimrc ${HOME}/.vimrc'
+ssh "${USER}@${HOST_TO_DEPLOY}" 'ln -s ${HOME}/.dotfiles/bin ${HOME}/bin'
+ssh "${USER}@${HOST_TO_DEPLOY}" 'ln -s ${HOME}/.dotfiles/.bash_profile_remote ${HOME}/.bash_profile'
 cat ~/.ssh/id_rsa.pub | ssh "${USER}@${HOST_TO_DEPLOY}" 'cat >> .ssh/authorized_keys'
 ssh "${USER}@${HOST_TO_DEPLOY}" "chmod 700 .ssh; chmod 640 .ssh/authorized_keys"
 echo "alias ${ALIAS}='ssh ${USER}@${HOST_TO_DEPLOY}'" >> /tmp/new_aliases
