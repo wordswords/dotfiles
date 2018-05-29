0f71156f7e516a4e89cecbffab786e72424210da
David Craddock <david.craddock@arm.com>
2018-05-29 18:25:13 +0100

    Updates

---
+++ deploy-iterm2-default-profile.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -xe
+sed -i -e "s/davcra01/${USER}/g" com.googlecode.iterm2.plist
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "/Users/${USER}/.dotfiles/iterm2"
+defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+open /Applications/iTerm.app
+
