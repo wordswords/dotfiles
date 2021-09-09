6991101c326174ac7006c8370d69074ce62bd6d5
David Craddock <wordswords@users.noreply.github.com>
2021-09-09 10:57:54 +0100

    Update deploy-part-2.sh

--- deploy-part-2.sh
+++ deploy-part-2.sh
@@ -67,6 +67,7 @@ ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/.vim ~/.vim
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
+ln -s ~/.dotfiles/coc-settings.json ~/.vim/coc-settings.json
 
 report_progress 2 'Installing vim plugins latest version..'
 rm -rf ~/.dotfiles/.vim/bundle/*
@@ -137,9 +138,6 @@ then
 fi
 # Change extension names to the extensions you need
 npm install coc-snippets coc-tsserver coc-json coc-html coc-css coc-python --global-style --ignore-scripts --no-bin-links --no-package-lock --only=prod
-
-# Install coc-settings file
-cp ~/.dotfiles/coc-settings.json ~/.vim/coc-settings.json
 
 report_progress 2 'Installing vim colorscheme..'
 git clone https://github.com/shannonmoeller/vim-monokai256 ./colorscheme
