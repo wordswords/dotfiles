4c6f73a9fd0ee522b503074425702e962f22e180
David Craddock <wordswords@users.noreply.github.com>
2021-09-09 05:33:31 +0100

    Delete check-jira.sh

--- bin/check-jira.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -e
-curl -o ./jira-json-dump.json -s -u $USER -X GET -H "Content-Type: application/json" https://jira.arm.com/rest/api/2/search\?jql\=assignee\=$USER\&maxResults\=9999
-jq -r '.issues[] | (.key),(.fields | (.status.name),(.summary))' ./jira-json-dump.json | paste - /dev/null - /dev/null -  | grep -v 'Closed'
