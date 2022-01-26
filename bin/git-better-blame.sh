b735b97b8568a2194a9bfbc40f8ac06340cf45ae
David Craddock <contact@davidcraddock.net>
2022-01-26 21:21:06 +0000

    Amazingly useful git blame replacment
    Snagged from: https://www.reddit.com/r/git/comments/9iqqs6/git_blame_show_commit_message/

---
+++ bin/git-better-blame.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+declare sha line key value
+declare -A kv
+
+unset GIT_PAGER
+[[ -t 1 ]] && GIT_PAGER=$(git var GIT_PAGER || :)
+
+unset abbrev
+abbrev=$(git config core.abbrev || :)
+[[ $abbrev == +([0-9]) ]] || abbrev=10
+
+git blame --line-porcelain "$@" | {
+    while read -r sha _; do
+        kv=()
+        unset line
+        while IFS= read -r line; do
+            [[ $line != $'\t'* ]] || break
+            read -r key value <<<"$line"
+            kv[$key]=$value
+        done
+        [[ -n ${line:+set} ]] || break
+        printf "%-${abbrev}.${abbrev}s (%-40.40s) %s\\n" "$sha" "${kv[summary]:-}" "${line#$'\t'}"
+    done
+} | eval "${GIT_PAGER:-cat}"
+
+
