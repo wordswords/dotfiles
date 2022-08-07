fd2f2a3b0a9775973d822357a796c46cacf8e613
David Craddock <contact@davidcraddock.net>
2022-08-07 22:30:25 +0100

    Enable unattended updates

--- deploy-part-0.sh
+++ deploy-part-0.sh
@@ -7,5 +7,10 @@ echo "We will now attempt to install the preqs under Ubuntu-compatible systems."
 sudo apt install vim git python3 python3-pip openjdk-18-jdk zsh ecryptfs-utils
 exuberant-ctags net-tools fortune-mod fortune-ubuntu-server
 
+echo "We will now attempt to enable automated unattended-upgrades"
+sudo apt-get install unattended-upgrades
+sudo dpkg-reconfigure unattended-upgrades
+
+
 
 
