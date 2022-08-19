b6d36906ba4c695f9521d3e346abfc6add4d06d4
David Craddock <contact@davidcraddock.net>
2022-08-19 21:59:27 +0100

    Document and update Ubuntu package install script

--- deploy-part-0.sh
+++ deploy-part-0.sh
@@ -4,10 +4,31 @@ set -e
 
 echo "We will now attempt to install the preqs under Ubuntu-compatible systems."
 
-sudo apt install vim git python3 python3-pip openjdk-18-jdk zsh ecryptfs-utils exuberant-ctags net-tools fortune-mod fortunes-ubuntu-server ripgrep
+sudo apt install vim git
+# Python 3 used for vim plugins
+sudo apt install python3
+sudo apt install python3-pip
+# Java open jdk, used for LanguageTool checker
+sudo apt install openjdk-18-jdk
+# zsh the best shell (so far)
+sudo apt install zsh
+# Used for encrypted .secure dir
+sudo apt install ecryptfs-utils
+# Ctags used for fugative 
+sudo apt install exuberant-ctags
+# net-tools used for?
+sudo apt install net-tools
+# Install fortune cookie for shell login
+sudo apt install fortune-mod
+# Ubuntu CLI command fortunes
+sudo apt install fortunes-ubuntu-server
+# Ripgrep used for :CocSearch
+sudo apt install ripgrep
+# tree is very useful for showing directory structures'
+sudo apt install tree
 
 echo "We will now attempt to enable automated unattended-upgrades"
-sudo apt-get install unattended-upgrades
+sudo apt install unattended-upgrades
 sudo dpkg-reconfigure unattended-upgrades
 
 
