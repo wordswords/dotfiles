d034c9d212064833fb20e52a816da7a9619db175
David Craddock <contact@davidcraddock.net>
2022-09-01 01:37:23 +0100

    Nicer report colours etc

--- deploy-common.sh
+++ deploy-common.sh
@@ -7,7 +7,7 @@ git config --global http.sslVerify false
 
 report_heading () {
     message="$1"
-    echo -e "\e[0;31m[**] ${message} [**]\e[0m"
+    echo -e "\e[0;31m[!] ${message} [!]\e[0m"
 }
 
 write_message () {
@@ -23,13 +23,13 @@ remove_message () {
 report_progress () {
   message=$1
   echo
-  echo -e "\e[0;36m?? ${message}\e[0m"
+  echo -e "\e[0;36m[?] ${message}\e[0m"
   write_message "${message}"
 }
 
 report_done () {
   message=$(</tmp/report_progress_message/message.txt)
-  echo -e "\e[0;36m++ ${message} ... \e[0;32m[DONE]\e[0m"
+  echo -e "\e[0;32m[+]\e[0m \e[0;36m${message} ... \e[0m\e[0;32m[DONE]\e[0m"
   remove_message
 }
 
