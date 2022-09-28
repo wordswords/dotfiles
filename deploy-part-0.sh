ec515d5d052272270c625de9f5724ab5dc10e8af
David Craddock <contact@davidcraddock.net>
2022-09-28 01:07:14 +0100

    Made deploy preqs script not prompt

--- deploy-part-0.sh
+++ deploy-part-0.sh
@@ -7,35 +7,36 @@ source ./deploy-common.sh
 report_heading 'Deploy Prerequisites: Part 0'
 
 report_progress 'We will now attempt to install the preqs under Ubuntu-compatible systems.'
-
+# fresh start
+sudo apt update -y && sudo apt upgrade -y && sudo apt autoremove -y
 # vim9
-sudo add-apt-repository ppa:jonathonf/vim
-sudo apt update && sudo apt upgrade
-sudo apt upgrade vim
+sudo add-apt-repository ppa:jonathonf/vim -y
+sudo apt update -y && sudo apt upgrade -y
+sudo apt upgrade vim -y
 # speedtest-cli
 curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
 sudo apt-get install speedtest
 # git
-sudo apt install git
+sudo apt install git -y
 # Python 3 used for vim plugins
-sudo apt install python3
-sudo apt install python3-pip
+sudo apt install python3 -y
+sudo apt install python3-pip -y
 # Java open jdk, used for LanguageTool checker
-sudo apt install default-jdk
+sudo apt install default-jdk -y
 # zsh the best shell (so far)
-sudo apt install zsh
+sudo apt install zsh -y
 # Used for encrypted .secure dir
-sudo apt install ecryptfs-utils
+sudo apt install ecryptfs-utils -y
 # Ctags used for fugative 
-sudo apt install exuberant-ctags
+sudo apt install exuberant-ctags -y
 # net-tools used for?
-sudo apt install net-tools
+sudo apt install net-tools -y
 # Install fortune cookie for shell login
-sudo apt install fortune-mod
+sudo apt install fortune-mod -y
 # Ripgrep used for :CocSearch
-sudo apt install ripgrep
+sudo apt install ripgrep -y
 # tree is very useful for showing directory structures'
-sudo apt install tree
+sudo apt install tree -y
 report_done
 
 report_progress 'We will now attempt to enable automated unattended-upgrades'
