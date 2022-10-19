a02c5d9b2a34366e0a65871bcbacbfd114f943ba
David Craddock <contact@davidcraddock.net>
2022-10-19 18:11:36 +0100

    Some fixes to make it work properly

--- deploy-part-4.zsh
+++ deploy-part-4.zsh
@@ -2,21 +2,28 @@
 
 set -e
 
+# uninstall
+rm -rf ./lieer
+rm -rf ${HOME}/mail
+rm -rf ${HOME}/.mail
+
 # install lieer+notmuch
+mkdir -p ${HOME}/mail
+
 git clone git@github.com:gauteh/lieer.git
 cd lieer
 pip install notmuch
 pip install google_api_python_client
 pip install oauth2client
 pip install tqdm
-pip install from git+ssh://git@github.com/weilbith/notmuch2-python-bindings
+#pip install from git+ssh://git@github.com/weilbith/notmuch2-python-bindings
 sudo apt install python3-notmuch2
 sudo apt install notmuch
 pip install .
 
 # configure notmuch
-notmuch setup
 notmuch new
+notmuch setup
 
 # configure lieer
 mkdir -p ~/.mail/account.gmail
