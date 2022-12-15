2db81f850b79f67beefb0cb411ebb598bdfa35e3
David Craddock <contact@davidcraddock.net>
2022-12-15 07:25:03 +0000

    Fixed

--- backup/rclone-backup-games.sh
+++ backup/rclone-backup-games.sh
@@ -1,2 +1,2 @@
-rclone copy /mnt2/games gdrive:rclone-backup/games -P --fast-list && rclone copy /mnt2/gamesincoming gdrive:rclone-backup/gamesincoming -P --fast-list &
+rclone copy /mnt/games gdrive:rclone-backup/games -P --fast-list && rclone copy /mnt/gamesincoming gdrive:rclone-backup/gamesincoming -P --fast-list &
 
