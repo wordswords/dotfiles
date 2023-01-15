53e52ad30ccbe34847f0585d5be9f6d20f5ac805
David Craddock <contact@davidcraddock.net>
2023-01-15 19:03:52 +0000

    Useful ChatGPT script to figure out dir purpose

---
+++ bin/ai-guess-dir-purpose.zsh
@@ -0,0 +1,6 @@
+#!/bin/zsh 
+
+set -e
+alias pbcopy='xclip -selection clipboard'
+find . -maxdepth 1 -print0 | xargs -0 echo | openai_pipe 'what is your best guess at the purpose of the files in this directory, given their naming?'
+
