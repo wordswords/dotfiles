d51805958321a524fd40ff2825ee7375ecebf2d8
David Craddock <contact@davidcraddock.net>
2023-03-26 04:58:32 +0100

    Fixed beet import script

--- music-organisation/beet-import.sh
+++ music-organisation/beet-import.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
 set -e
+set -x
 
+# Make sure we are running the latest version
+sudo pip3 install --upgrade beets
+
+# Fix perms
+
+USER=`whoami`
+sudo chown -R $USER:$USER /mnt2/incomingmusic
+sudo chown -R $USER:$USER /mnt2/music
+sudo chmod -R 775 /mnt2/incomingmusic
+sudo chmod -R 775 /mnt2/music
+
+# Import music
 cd /mnt2/incomingmusic
 beet import -AmPsq .
 beet dup -d
+
+# Clean incoming directory
 sudo rm -rf /mnt2/incomingmusic/*
+
