d67602f195bc1acfad775e5c8b3a77611327f5d5
David Craddock <contact@davidcraddock.net>
2023-03-30 03:40:03 +0100

    Fixed problem with perms in backup

--- backup/backup-all.sh
+++ backup/backup-all.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
-
-set -x
 set -e
 
-sudo ./borg-backup-server-config.sh
-sudo -u david cd ~/.dotfiles/backup && ./rclone-backup-borg.sh
-sudo -u david cd ~/.dotfiles/backup && ./rclone-backup-ebooks.sh
-sudo -u david cd ~/.dotfiles/backup && ./rclone-backup-music.sh
-sudo -u david cd ~/.dotfiles/backup && ./rclone-backup-TV.sh
-sudo -u david cd ~/.dotfiles/backup && ./rclone-backup-games.sh
-sudo -u david cd ~/.dotfiles/backup && ./rclone-backup-video.sh
+sudo ./borg-backup-server-config.sh >/dev/null 2>/dev/null
+sudo -u david cd ~/.dotfiles/backup && ./rclone-backup-borg.sh >/dev/null 2>/dev/null
+sudo -u david cd ~/.dotfiles/backup && ./rclone-backup-ebooks.sh >/dev/null 2>/dev/null
+sudo -u david cd ~/.dotfiles/backup && ./rclone-backup-music.sh >/dev/null 2>/dev/null
+sudo -u david cd ~/.dotfiles/backup && ./rclone-backup-TV.sh >/dev/null 2>/dev/null
+sudo -u david cd ~/.dotfiles/backup && ./rclone-backup-games.sh >/dev/null 2>/dev/null
+sudo -u david cd ~/.dotfiles/backup && ./rclone-backup-video.sh >/dev/null 2>/dev/null
 
 # if it gets this far, it's successful
-sudo echo "Last backup success: `date`" > ~/.dotfiles/backup/.last_successful_backup
-chmod 700 /home/david/.dotfiles/backup/.last_successful_backup
-chown david:david ~/.dotfiles/backup/.last_successful_backup
+sudo -u david echo "Last backup success: `date`" > ~/.dotfiles/backup/.last_successful_backup 2>/dev/null
+sudo -u david chmod 700 /home/david/.dotfiles/backup/.last_successful_backup >/dev/null 2>/dev/null
 
 # update the git repo
-sudo -u david git add ~/.dotfiles/backup/.last_successful_backup && git commit -m 'Updated last successful backup' && git push -f |&>/dev/null
+sudo -u david git add ~/.dotfiles/backup/.last_successful_backup && git commit -m 'Updated last successful backup' && git push -f >/dev/null 2>/dev/null
+
 
