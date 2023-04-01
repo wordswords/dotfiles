7d7e4683a85036e50ae67d505b4c0696e7fe7fcf
David Craddock <contact@davidcraddock.net>
2023-04-01 16:01:22 +0100

    Fixed cron backup job

--- backup/backup-all.sh
+++ backup/backup-all.sh
@@ -13,12 +13,11 @@ runuser -u david /home/david/.dotfiles/backup/rclone-backup-games.sh
 runuser -u david /home/david/.dotfiles/backup/rclone-backup-video.sh 
 
 # if it gets this far, it's successful
-
 echo Last backup success: $(date) > /home/david/.dotfiles/backup/.last_successful_backup
 chown -R david:david /home/david/.dotfiles
 
 # update the git repo
-sudo -H -u david -i /home/david/.dotfiles/backup/save-last-backedup.sh
+runuser -u david /home/david/.dotfiles/backup/save-last-backedup.sh
 
 
 
