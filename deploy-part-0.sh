c0468447459e0f58e538ef81ed76301f3cfc4f97
David Craddock <contact@davidcraddock.net>
2023-04-13 17:16:06 +0100

    Fixed google cloud sdk

--- deploy-part-0.sh
+++ deploy-part-0.sh
@@ -125,7 +125,7 @@ report_done
 
 report_progress 'Installing Google Cloud CLI client'
     sudo apt-get install apt-transport-https ca-certificates gnupg
-    echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt-getcloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+    echo "deb https://packages.cloud.google.com/apt-getcloud-sdk jammy main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
     curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
     sudo apt-get update && sudo apt-get install google-cloud-cli -y
 report_done
