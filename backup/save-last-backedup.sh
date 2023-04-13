cd684e0510cdbc4da910b1d924e90930c8798955
David Craddock <contact@davidcraddock.net>
2023-04-13 16:02:55 +0100

    Fixed backup

--- backup/save-last-backedup.sh
+++ backup/save-last-backedup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+set +e
 git config --global --add safe.directory /home/david/.dotfiles
 cd /home/david/.dotfiles/backup
 git add .last_successful_backup
 git commit -m 'Updated last successful backup'
 git push -f
-
+set -e
 
