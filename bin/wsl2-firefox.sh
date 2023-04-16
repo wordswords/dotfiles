5e20b881bb6fffbe820b8c5c434a1b5d2bafff5a
David Craddock <contact@davidcraddock.net>
2023-04-16 01:22:32 +0100

    Updated p10k and added firefox wsl2 install

---
+++ bin/wsl2-firefox.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+set -x
+
+# deal with the horribleness that is Firefox under WSL2
+
+sudo snap remove firefox || true
+sudo apt remove firefox || true
+sudo add-apt-repository ppa:mozillateam/ppa || true
+
+# Insert these lines, then save and exit
+echo '''
+Package: firefox*
+Pin: release o=LP-PPA-mozillateam
+Pin-Priority: 501
+''' > /tmp/mozillateamppa
+sudo mv /tmp/mozillateamppa /etc/apt/preferences.d/mozillateamppa
+
+# after saving, do
+sudo apt update
+sudo apt install firefox # or firefox-esr
+
+# hack to make firefox work with other scripts
+sudo ln -s /bin/firefox /snap/bin/firefox
