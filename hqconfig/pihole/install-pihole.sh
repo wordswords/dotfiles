183cddb317524bfb17efc12d0edd5d14b9777cec
David Craddock <contact@davidcraddock.net>
2023-04-16 02:11:34 +0100

    Update cron.weekly install script

--- hqconfig/pihole/install-pihole.sh
+++ hqconfig/pihole/install-pihole.sh
@@ -3,6 +3,6 @@
 set -e
 set -x
 
-sudo curl -O /etc/cron.d/cron.weekly/docker-pi-hole https://raw.githubusercontent.com/pi-hole/docker-pi-hole/master/examples/docker-pi-hole.cron
-sudo chmod 700 /etc/cron.d/cron.weekly/docker-pi-hole
+sudo wget -O /etc/cron.weekly/docker-pi-hole https://raw.githubusercontent.com/pi-hole/docker-pi-hole/master/examples/docker-pi-hole.cron
+sudo chmod 700 /etc/cron.weekly/docker-pi-hole
 
