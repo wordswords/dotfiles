27ac5d73336bfd5c54e462f4a9daaff43b7aec4c
David Craddock <contact@davidcraddock.net>
2023-04-29 00:35:02 +0100

    Automated

--- hqconfig/start-hq-docker-services.sh
+++ hqconfig/start-hq-docker-services.sh
@@ -11,3 +11,21 @@ cd -
 cd ./explainshell/explainshell/
 docker-compose up -d
 cd -
+
+cd ./automaticrippingmachine
+docker-compose up -d
+cd -
+
+cd ./prowlarr
+docker-compose up -d
+cd -
+
+cd ./lidarr
+docker-compose up -d
+cd -
+
+cd ./qbtorrent
+docker-compose up -d
+cd -
+
+
