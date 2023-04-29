73f6475d0b948e4831a86284a8c00243c46636a8
David Craddock <contact@davidcraddock.net>
2023-04-29 23:51:13 +0100

    With proper error handling

--- backup/backup-all.sh
+++ backup/backup-all.sh
@@ -1,32 +1,49 @@
 #!/bin/bash
+set -e
+set -x
+
+
 if [ "$(id -u)" -ne 0 ] ; then
     echo 'You must be root to run this script'
     exit 1
 fi
 
-# Screen setup
-echo "defscrollback 1000000" > ~/.screenrc
-if [ -z "$STY" ]; then
-    exec screen -dm -S backup-all "/bin/bash $0 || read -p 'Backup failed. Press enter to continue.'";
-fi
 
-set -e
-set -x
+error() {
+  local parent_lineno="$1"
+  local message="$2"
+  local code="${3:-1}"
+  if [[ -n "$message" ]] ; then
+    echo "Error on or near line ${parent_lineno}: ${message}; exiting with status ${code}"
+  else
+    echo "Error on or near line ${parent_lineno}; exiting with status ${code}"
+  fi
+  read -p "** Press any key to exit **"
+  exit "${code}"
+}
+trap 'error ${LINENO}' ERR
 
-cd /home/david/.dotfiles/backup/
+backupall() {
+	# Screen setup
+	echo "defscrollback 1000000" > ~/.screenrc
+	if [ -z "$STY" ]; then
+	    exec screen -dm -S backup-all /bin/bash "$0";
+	fi
 
-./borg-backup-server-config.sh
-chown -R david:david /mnt2/borg-backup
-runuser -u david /home/david/.dotfiles/backup/rclone-backup-borg.sh
+	cd /home/david/.dotfiles/backup/
 
-# if it gets this far, it's successful
-echo Last backup success: "$(date)" > /home/david/.dotfiles/backup/.last_successful_backup
-chown -R david:david /home/david/.dotfiles
+	./borg-backup-server-config.sh
+	chown -R david:david /mnt2/borg-backup
+	runuser -u david /home/david/.dotfiles/backup/rclone-backup-borg.sh
 
-# update the git repo
-runuser -u david /home/david/.dotfiles/backup/save-last-backedup.sh
+	# if it gets this far, it's successful
+	echo Last backup success: "$(date)" > /home/david/.dotfiles/backup/.last_successful_backup
+	chown -R david:david /home/david/.dotfiles
 
-hardcopy -h /home/david/.dotfiles/backup/.last_backup_all_run.log
+	# update the git repo
+	runuser -u david /home/david/.dotfiles/backup/save-last-backedup.sh
 
-
+	hardcopy -h /home/david/.dotfiles/backup/.last_backup_all_run.log
+}
+trap backupall 0
 
