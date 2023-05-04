ca17c2099e5704370d659622465213334db10d2d
David Craddock <contact@davidcraddock.net>
2023-05-04 17:52:09 +0100

    Updated with static ip address fix

--- hqconfig/start-hq-docker-services.sh
+++ hqconfig/start-hq-docker-services.sh
@@ -15,15 +15,43 @@ function updateAndUp() {
 }
 function JustUp() {
     cd "$1"
-    docker compose up -d
+    docker-compose up -d
     cd -
 }
-JustUp ./pihole
-updateAndUp ./explainshell/explainshell
-updateAndUp ./prowlarr
-updateAndUp ./lidarr
-updateAndUp ./qbtorrent
-updateAndUp ./calibreweb
+function JustDown() {
+    cd "$1"
+    docker-compose down || true
+    cd -
+}
+function JustUpdate() {
+    cd "$1"
+    docker-compose pull
+    cd -
+}
+function PruneNetworks() {
+    docker network prune -f
+}
+function PruneUnusuedContainers() {
+    docker image prune -a -f
+}
 
+JustUp ./pihole
+JustDown ./explainshell/explainshell
+JustDown ./prowlarr
+JustDown ./qbtorrent
+JustDown ./lidarr
+JustDown ./calibreweb
+PruneUnusuedContainers
+PruneNetworks
+JustUpdate ./explainshell/explainshell
+JustUpdate ./lidarr
+JustUpdate ./prowlarr
+JustUpdate ./qbtorrent
+JustUpdate ./calibreweb
+JustUp ./explainshell/explainshell
+JustUp ./lidarr
+JustUp ./prowlarr
+JustUp ./qbtorrent
+JustUp ./calibreweb
 
 
