969e344984ce8d8d980b310a0625806306ba7065
David Craddock <contact@davidcraddock.net>
2023-05-29 17:16:23 +0100

    Fixed

--- backup/backup-all.sh
+++ backup/backup-all.sh
@@ -11,6 +11,23 @@ exec > >(tee "$logfile") 2>&1
 set -x
 set -e
 
+backupall() {
+    trap 'error ${LINENO}' ERR
+	# Screen setup
+	echo "defscrollback 1000000" > ~/.screenrc
+
+	cd /home/david/.dotfiles/backup/
+	./borg-backup-server-config.sh
+	chown -R david:david /mnt2/borg-backup
+	sudo -u david /home/david/.dotfiles/backup/rclone-backup-borg.sh
+
+	# if it gets this far, it's successful
+    sudo -u david echo Last backup success: "$(date)" > /home/david/.dotfiles/backup/.last_successful_backup
+	chown -R david:david /home/david/.dotfiles
+
+	# update the git repo
+	sudo -u david /home/david/.dotfiles/backup/save-last-backedup.sh
+}
 error() {
   local parent_lineno="$1"
   local message="$2"
@@ -23,23 +40,6 @@ error() {
   read -p "** Press any key to exit **"
   exit "${code}"
 }
-trap 'error ${LINENO}' ERR
 
-backupall() {
-	# Screen setup
-	echo "defscrollback 1000000" > ~/.screenrc
-
-	cd /home/david/.dotfiles/backup/
-	./borg-backup-server-config.sh
-	chown -R david:david /mnt2/borg-backup
-	runuser -u david /home/david/.dotfiles/backup/rclone-backup-borg.sh
-
-	# if it gets this far, it's successful
-	echo Last backup success: "$(date)" > /home/david/.dotfiles/backup/.last_successful_backup
-	chown -R david:david /home/david/.dotfiles
-
-	# update the git repo
-	runuser -u david /home/david/.dotfiles/backup/save-last-backedup.sh
-}
-trap backupall 0
+backupall
 
