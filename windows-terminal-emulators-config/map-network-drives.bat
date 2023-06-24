cd81f4da6f4dc07660931383790d9b411a0efb94
David Craddock <contact@davidcraddock.net>
2023-06-24 22:50:50 +0100

    Final non-conflicting version

--- windows-terminal-emulators-config/map-network-drives.bat
+++ windows-terminal-emulators-config/map-network-drives.bat
@@ -1,11 +1,11 @@
-net use X: \\hq.local\games
 net use I: \\hq.local\incominggames
 net use J: \\hq.local\incomingmusic
 net use K: \\hq.local\incomingtvvideo
 net use M: \\hq.local\music
 net use N: \\hq.local\incomingebooks
 net use O: \\hq.local\incomingcomics
-net use S: \\hq.local\incomingscratch
+net use X: \\hq.local\games
+net use Z: \\hq.local\incomingscratch
 
 
 
