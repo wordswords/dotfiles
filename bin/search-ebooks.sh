b1b730a4dcd801ba7cd7947f6f485c6e16dce4b2
David Craddock <contact@davidcraddock.net>
2023-08-31 19:14:57 +0100

    Updated for remote execution

--- bin/search-ebooks.sh
+++ bin/search-ebooks.sh
@@ -3,8 +3,9 @@
 set -e
 BOOKPATHROOT=/mnt/ebooks
 PARAMS="*$**.epub"
+EPY_PATH="/home/david/.local/bin/epy"
 
 cd "${BOOKPATHROOT}"
 bookpath=$(find . -type f -iname "${PARAMS}" 2>/dev/null | sort -r | fzf --disabled)
-epy "${bookpath}"
+"${EPY_PATH}" "${bookpath}"
 cd -
