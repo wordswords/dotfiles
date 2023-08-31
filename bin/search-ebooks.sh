46ec1bbf576c0d9e6b0882f04307712845b9a200
David Craddock <contact@davidcraddock.net>
2023-08-31 19:00:48 +0100

    Get rid of some error rubbish

--- bin/search-ebooks.sh
+++ bin/search-ebooks.sh
@@ -4,6 +4,6 @@ set -e
 BOOKPATHROOT=/mnt/ebooks
 
 params="*$**.epub"
-bookpath=$(find "${BOOKPATHROOT}" -type f -iname "${params}" | fzf --disabled)
+bookpath=$(find "${BOOKPATHROOT}" -type f -iname "${params}" 2>/dev/null | fzf --disabled)
 epy "${bookpath}"
 
