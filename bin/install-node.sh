c81789fb00e07527916f94723c6a384f0d979b38
David Craddock <contact@davidcraddock.net>
2023-12-13 23:14:10 +0000

    Fix small node prob

--- bin/install-node.sh
+++ bin/install-node.sh
@@ -4,6 +4,7 @@ set -e
 set -x
 
 # Install node via Nodesource
+NODE_MAJOR="$1" # version of node to install!
 
 if [ $# -eq 0 ]; then
     >&2 echo "Usage: $0 <node major version>"
@@ -15,8 +16,7 @@ sudo apt-get install -y ca-certificates curl gnupg
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --yes --dearmor -o /etc/apt/keyrings/nodesource.gpg
 
-NODE_MAJOR="$1" # version of node to install!
-sudo echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_${NODE_MAJOR}.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 sudo cat /etc/apt/sources.list.d/nodesource.list | sort -u | sudo tee /etc/apt/sources.list.d/nodesource.list.dedupe
 sudo mv /etc/apt/sources.list.d/nodesource.list.dedupe /etc/apt/sources.list.d/nodesource.list
 
