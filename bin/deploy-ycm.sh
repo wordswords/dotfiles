e3f58916871bf871b108bfdfcc8586b42d2547f8
David Craddock <contact@davidcraddock.net>
2024-01-07 03:56:27 +0000

    Fixed!

--- bin/deploy-ycm.sh
+++ bin/deploy-ycm.sh
@@ -8,7 +8,7 @@ sudo apt-get install build-essential python3-dev libssl-dev
 wget https://github.com/Kitware/CMake/releases/download/v3.28.0/cmake-3.28.0.tar.gz
 tar xzf cmake-3.28.0.tar.gz
 cd ./cmake-3.28.0
-./bootstrap && make && sudo make install
+./bootstrap && make clean && make && sudo make install
 cd -
 rm -rf ./cmake*
 
@@ -16,10 +16,11 @@ rm -rf ./cmake*
 sudo apt-get install -y mono-complete golang openjdk-19-jdk openjdk-19-jre
 
 # Install YCM
-rm -rf ~/.vim/bundle/YouCompleteMe/* | true
+rm -rf ~/.vim/bundle/YouCompleteMe | true
 mkdir -p ~/.vim/bundle/YouCompleteMe
 chmod -R 700 ~/.vim/bundle/YouCompleteMe/ | true
-git clone git@github.com:ycm-core/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe
+cd ~/.vim/bundle/YouCompleteMe/
+git clone git@github.com:ycm-core/YouCompleteMe.git ./
 cd ~/.vim/bundle/YouCompleteMe/
 git submodule update --init --recursive
 python3 install.py --all
