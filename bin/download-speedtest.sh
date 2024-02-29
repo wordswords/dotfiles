5d7d1f6663bca9c5103aac8bba3feee5c354eeca
David Craddock <contact@davidcraddock.net>
2024-02-29 21:03:52 +0000

    Added download speedtest script

---
+++ bin/download-speedtest.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -x
+set -e
+
+speedtestver="1.2.0"
+tmpdir=$(mktemp -d)
+cd "${tmpdir}"
+wget https://install.speedtest.net/app/cli/ookla-speedtest-${speedtestver}-linux-x86_64.tgz
+tar xzf ookla-speedtest-*.tgz
+mv speedtest ~/bin/
+rm -rf "${tmpdir}"
+
