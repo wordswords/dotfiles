eafbe7d1321933a7a71567cf66bfe8c780573554
David Craddock <contact@davidcraddock.net>
2024-03-19 12:56:19 +0000

    Install fnm

--- deploy-part-0.zsh
+++ deploy-part-0.zsh
@@ -185,6 +185,9 @@ report_done
 report_progress 'Install speedtest from Okla'
     ~/.dotfiles/bin/download-speedtest.sh
 report_done
+report_progress 'Install fnm node.js version manager'
+    ~/.dotfiles/bin/install-fnm.sh
+report_done
 
 # custom installation lines
 report_progress 'Installing McFly, a zsh Control-R replacement'
