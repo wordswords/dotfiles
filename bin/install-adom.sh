6c7bb8681c7b704816464ac3bc3ec7cf08e60bad
David Craddock <contact@davidcraddock.net>
2024-04-12 07:30:45 +0100

    Fix for adom install

--- bin/install-adom.sh
+++ bin/install-adom.sh
@@ -7,7 +7,7 @@ set -e
 
 sudo apt install libncurses5 -y
 wget https://www.adom.de/home/download/current/adom_linux_ubuntu_64_3.3.3.tar.gz
-tar zxf adom_linux*
-cp ./adom/adom ~/bin
+tar xzf adom* ./adom
+cp ./adom*/adom ~/bin
 rm -rf ./adom*
 
