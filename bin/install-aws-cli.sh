48f18e9d5e68da5723033a3cfc807eb235f795d9
David Craddock <contact@davidcraddock.net>
2024-04-20 22:45:26 +0100

    Install aws cli on linux

---
+++ bin/install-aws-cli.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -x
+set -e
+
+tmpdir=$(mktemp -d)
+cd ${tmp}
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+if [ -d /usr/local/aws-cli ]; then
+    sudo ./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update
+else
+    sudo ./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli
+fi
+cd -
+rm -rf ${tmp}
+
