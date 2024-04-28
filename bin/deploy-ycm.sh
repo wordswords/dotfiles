2691e65283fca3641c381a024ca73d696c009a96
David Craddock <contact@davidcraddock.net>
2024-04-28 01:47:56 +0100

    Made it so that it skips cmake step if done before

--- bin/deploy-ycm.sh
+++ bin/deploy-ycm.sh
@@ -4,19 +4,20 @@ set -e
 set -x
 
 # Install Cmake 3.28.0
-sudo apt-get install build-essential python3-dev libssl-dev -y
-wget https://github.com/Kitware/CMake/releases/download/v3.28.0/cmake-3.28.0.tar.gz
-tar xzf cmake-3.28.0.tar.gz
-cd ./cmake-3.28.0
-./bootstrap && make clean && make && sudo make install
-cd -
-rm -rf ./cmake*
+if [ ! -d ./cmake-3.28.0 ] ; then
+    sudo apt-get install build-essential python3-dev libssl-dev -y
+    wget https://github.com/Kitware/CMake/releases/download/v3.28.0/cmake-3.28.0.tar.gz
+    tar xzf cmake-3.28.0.tar.gz
+    cd ./cmake-3.28.0
+    ./bootstrap && make clean && make && sudo make install
+    cd -
+fi
 
 # Assumes node is already installed - Installs all other prereqs for YCM
 sudo apt-get install -y mono-complete golang openjdk-19-jdk openjdk-19-jre
 
 # Install YCM
-rm -rf ~/.vim/bundle/YouCompleteMe | true
+sudo rm -rf ~/.vim/bundle/YouCompleteMe | true
 mkdir -p ~/.vim/bundle/YouCompleteMe
 chmod -R 700 ~/.vim/bundle/YouCompleteMe/ | true
 cd ~/.vim/bundle/YouCompleteMe/
