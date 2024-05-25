ec98d69e9222e2548e21efa2b5fa26ed99ee8a25
David Craddock <contact@davidcraddock.net>
2024-05-25 20:44:24 +0100

    Add winbox for Linux setup

---
+++ bin/linux-winbox.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+set -x
+sudo apt-get install wine64 -y
+sudo ufw allow 5678/udp
+sudo allow ufw from 0.0.0.0 to 255.255.255.255
+sudo ufw reload
+
+wine ~/.dotfiles/bin/winbox64.exe
+
+
