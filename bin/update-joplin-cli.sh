69e0bbc4f316437a11fbe1045f087325f1013abf
David Craddock <contact@davidcraddock.net>
2024-06-02 14:57:20 +0100

    Needs sudo

--- bin/update-joplin-cli.sh
+++ bin/update-joplin-cli.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo rm -rf ~/.joplin-bin
-sudo apt install joplin-cli || NPM_CONFIG_PREFIX=~/.joplin-bin npm install --force -g joplin
+sudo apt install joplin-cli || NPM_CONFIG_PREFIX=~/.joplin-bin sudo npm install --force -g joplin
 ln -sf ~/.joplin-bin/bin/joplin ~/bin/joplin
