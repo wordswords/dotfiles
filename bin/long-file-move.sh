eb7fc21b7bd0be2dc7bba5bb139aa4a3e81d84cd
David Craddock <contact@davidcraddock.net>
2024-06-15 19:25:20 +0100

    Fixed

--- bin/long-file-move.sh
+++ bin/long-file-move.sh
@@ -6,6 +6,6 @@ set -x
 SOURCE="$1"
 DESTINATION="$2"
 
-#sudo rsync -avzh --remove-source-files --progress "${SOURCE}" "${DESTINATION}"
+sudo rsync -avzh --remove-source-files --progress "${SOURCE}" "${DESTINATION}"
 sudo find "${SOURCE}" -type d -empty -delete
 
