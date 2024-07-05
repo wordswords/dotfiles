9f3f9af80efa290035509ea053249c64e7b41567
David Craddock <contact@davidcraddock.net>
2024-07-05 19:24:19 +0100

    Fixed because GO support is broken for now in YCM

--- bin/deploy-ycm.sh
+++ bin/deploy-ycm.sh
@@ -4,14 +4,14 @@ set -e
 set -x
 
 # Install Cmake 3.28.0
-if [ ! -d ./cmake-3.28.0 ] ; then
-    sudo apt-get install build-essential python3-dev libssl-dev -y
-    wget https://github.com/Kitware/CMake/releases/download/v3.28.0/cmake-3.28.0.tar.gz
-    tar xzf cmake-3.28.0.tar.gz
-    cd ./cmake-3.28.0
-    ./bootstrap && make clean && make && sudo make install
-    cd -
-fi
+#if [ ! -d ./cmake-3.28.0 ] ; then
+#    sudo apt-get install build-essential python3-dev libssl-dev -y
+#    wget https://github.com/Kitware/CMake/releases/download/v3.28.0/cmake-3.28.0.tar.gz
+#    tar xzf cmake-3.28.0.tar.gz
+#    cd ./cmake-3.28.0
+#    ./bootstrap && make clean && make && sudo make install
+#    cd -
+#fi
 
 # Assumes node is already installed - Installs all other prereqs for YCM
 sudo apt install -y mono-complete golang
@@ -25,5 +25,5 @@ cd ~/.vim/bundle/YouCompleteMe/
 git clone git@github.com:ycm-core/YouCompleteMe.git ./
 cd ~/.vim/bundle/YouCompleteMe/
 git submodule update --init --recursive
-python3 install.py --all
+python3 install.py --cs-completer --java-completer --ts-completer
 
