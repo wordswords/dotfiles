a5a340381caec13199d2e7d75c70548fe81be56a
David Craddock <contact@davidcraddock.net>
2026-08-23 13:04:29 +0100

    Latest updates

--- bin/install-adom.sh
+++ bin/install-adom.sh
@@ -3,11 +3,13 @@ set -euo pipefail
 IFS=$'\n\t'
 
 # Install ADOM - Ancient Domains of Mystery - my 'desert island' game
-
+# For desert island situations.
 # ADOM_VERSION can be set via the environment (exported by deploy.sh).
 ADOM_VERSION="${ADOM_VERSION:-3.3.3}"
 
 sudo dnf install -y ncurses-libs
+sudo dnf config-manager --set-enabled crb
+sudo dnf install ncurses-compat-libs -y
 wget "https://www.adom.de/home/download/current/adom_linux_ubuntu_64_${ADOM_VERSION}.tar.gz"
 tar xzf "adom_linux_ubuntu_64_${ADOM_VERSION}.tar.gz"
 cp ./adom*/adom ~/bin
