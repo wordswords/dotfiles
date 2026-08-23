f07cbbb8842b95e1cde7eefbef0c25d9935ad097
David Craddock <contact@davidcraddock.net>
2026-08-23 13:05:54 +0100

    Latest updates

--- bin/install-adom.sh
+++ bin/install-adom.sh
@@ -5,23 +5,17 @@ IFS=$'\n\t'
 # Install ADOM - Ancient Domains of Mystery - my 'desert island' game
 # For desert island situations.
 # ADOM_VERSION can be set via the environment (exported by deploy.sh).
+#
 ADOM_VERSION="${ADOM_VERSION:-3.3.3}"
 ARCHIVE="adom_linux_ubuntu_64_${ADOM_VERSION}.tar.gz"
 
 sudo dnf install -y ncurses-libs
-<<<<<<< HEAD
 sudo dnf config-manager --set-enabled crb
 sudo dnf install ncurses-compat-libs -y
 wget "https://www.adom.de/home/download/current/adom_linux_ubuntu_64_${ADOM_VERSION}.tar.gz"
 tar xzf "adom_linux_ubuntu_64_${ADOM_VERSION}.tar.gz"
 cp ./adom*/adom ~/bin
 rm -rf ./adom*
-||||||| 2388b87
-wget "https://www.adom.de/home/download/current/adom_linux_ubuntu_64_${ADOM_VERSION}.tar.gz"
-tar xzf "adom_linux_ubuntu_64_${ADOM_VERSION}.tar.gz"
-cp ./adom*/adom ~/bin
-rm -rf ./adom*
-=======
 wget "https://www.adom.de/home/download/current/${ARCHIVE}"
 tar xzf "${ARCHIVE}"
 
@@ -29,5 +23,4 @@ tar xzf "${ARCHIVE}"
 sudo install -m 755 ./adom*/adom "${HOME}/bin/adom"
 find . -maxdepth 1 -type d -name 'adom*' -exec rm -rf {} +
 rm -f "${ARCHIVE}"
->>>>>>> 332813a2530758c5b129d957b39ea7ff14679635
 
